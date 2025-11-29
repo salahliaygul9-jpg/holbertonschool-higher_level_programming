27a0c959e27a018f958cdcd869d4c3132ceaf7de
salahliaygul9-jpg <salahliaygul9@gmail.com>
2025-11-29 18:47:20 +0000

    Copy - Cut - Paste

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 word = "Holberton"
-middle_word = word[1:8]
+middle_word = word[1:-1]
 word_last_2 = word[-2:]
 word_first_3 = word[:3]
 print(f"First 3 letters: {word_first_3}")
