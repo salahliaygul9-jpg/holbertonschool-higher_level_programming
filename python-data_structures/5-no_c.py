2b64b7f8ce4e8fd1b95037c4032d4b7715e470d5
salahliaygul9-jpg <salahliaygul9@gmail.com>
2025-12-06 15:34:28 +0000

    Can you C me now?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,11 +3,11 @@
 def no_c(my_string):
     new_string = ""
     for i in range(len(my_string)):
-        if my_string[i] != "c" and my_string[i] != "c":
+        if my_string[i] != "c" and my_string[i] != "C":  
             new_string += my_string[i]
     return new_string
 
 if __name__ == "__main__":
-    my_string = "Best School"
+    my_string = "Chicago"
     result = no_c(my_string)
-    print(result)
+    print(result)  
